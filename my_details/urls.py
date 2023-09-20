64eaf3a7e4ec93520163205751445cd195cf8838
sivadeepkumar <sivadeepkumar3@gmail.com>
2023-09-21 00:20:30 +0530

    Second Commit

--- my_details/urls.py
+++ my_details/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('upstox/', upstox, name='upstox'),
-    path('upstox_login/', upstox_login, name='upstox_login'),
-    path('get_access_token/', get_access_token, name='get_access_token'),
+    path('', upstox_login, name='upstox_login'),
+    path('upstox/callback', get_access_token,name='get_access_token'),
     path('get_user_profile/', get_user_profile, name='get_user_profile'),
     path('get_orders/', get_orders, name='get_orders'),
     path('each_order/<int:id>/',each_order,name='each_order'),
 ] 
+
+
+#  upstox_redirect
--- my_details/views.py
+++ my_details/views.py
@@ -1,111 +1,65 @@
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# import requests
-# from upstox_client import *
-
-# # Create your views here.
-# access_token = 'eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIzOTQ5NjMiLCJqdGkiOiI2NTA4N2IxNThiYzM5NjIyNTMzMTljOGMiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE2OTUwNTQ2MTMsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTY5NTA3NDQwMH0.HliDFDrSMa84N8DaG2NoDl8wFwly0yG4mXHGQM9uLH0'
-
-
-# # upstox_app/views.py
-
-
-# def get_user_profile(request):
-#     api_endpoint = 'https://api-v2.upstox.com/user/profile'
-#     headers = {
-#         'accept': 'application/json',
-#         'Api-Version': '2.0',
-#         'Authorization': f'Bearer {access_token}'
-#     }
-#     response = requests.get(api_endpoint, headers=headers)
-
-#     if response.status_code == 200:
-#         user_profile_data = response.json()
-#         user_profile = user_profile_data['data']
-#         return JsonResponse({'user_profile': user_profile})
-#     else:
-#         return JsonResponse({'error': f"Error: {response.status_code} - {response.text}"}, status=500)
-
-
-# def get_orders(request):
-#     api_endpoint = 'https://api-v2.upstox.com/order/retrieve-all'
-#     headers = {
-#         'accept': 'application/json',
-#         'Api-Version': '2.0',
-#         'Authorization': f'Bearer {access_token}'
-#     }
-#     response = requests.get(api_endpoint, headers=headers)
-
-#     if response.status_code == 200:
-#         orders_data = response.json()
-#         orders = orders_data['data']
-#         return JsonResponse({'orders': orders})
-#     else:
-#         return JsonResponse({'error': f"Error: {response.status_code} - {response.text}"}, status=500)
-
-
-
-
-# def each_order(request,id):
-#     api_endpoint = f'https://api-v2.upstox.com/order/history?tag={id}'
-#     headers = {
-#         'accept': 'application/json',
-#         'Api-Version': '2.0',
-#         'Authorization': f'Bearer {access_token}'
-#     }
-#     response = requests.get(api_endpoint, headers=headers)
-
-#     if response.status_code == 200:
-#         orders_data = response.json()
-#         orders = orders_data['data']
-#         return JsonResponse({'orders': orders})
-#     else:
-#         return JsonResponse({'error': f"Error: {response.status_code} - {response.text}"}, status=500)
-
-
-
-
-
 from django.shortcuts import render, redirect
 from rest_framework.response import Response
-
 from django.http import JsonResponse
 import requests
 from upstox_client import *
 import pdb
-import os
-import urllib.parse
+# import urllib.parse
+from decouple import config
+from django.http import *
 
-# Create your views here.
-access_token = 'eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIzOTQ5NjMiLCJqdGkiOiI2NTA4N2IxNThiYzM5NjIyNTMzMTljOGMiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNBY3RpdmUiOnRydWUsInNjb3BlIjpbImludGVyYWN0aXZlIiwiaGlzdG9yaWNhbCJdLCJpYXQiOjE2OTUwNTQ2MTMsImlzcyI6InVkYXBpLWdhdGV3YXktc2VydmljZSIsImV4cCI6MTY5NTA3NDQwMH0.HliDFDrSMa84N8DaG2NoDl8wFwly0yG4mXHGQM9uLH0'
-redirect_url = urllib.parse.quote(f"{os.environ.get('UPSTOX_REDIRECT_URI')}",safe="")
-client_id = os.environ.get("UPSTOX_CLIENT_ID")
 # upstox_app/views.py
 def upstox_login(request):
-    response = requests.get(f'https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id={os.environ.get("UPSTOX_CLIENT_ID")}&redirect_uri={os.environ.get("UPSTOX_REDIRECT_URI")}')
-    # r = requests.get("https://api-v2.upstox.com/login/authorization/dialog?response_type=code&client_id={}&redirect_uri={}".format(client_id,urllib.parse.quote(f"{os.environ.get('UPSTOX_REDIRECT_URI')}",safe="")))
-    pdb.set_trace()
-    redirect_url = response.url
-    return redirect(redirect_url)
+    redirect_url = config('UPSTOX_REDIRECT_URI')
+    response = "https://api-v2.upstox.com/login/authorization/dialog?client_id={}&redirect_uri={}".format(config('UPSTOX_CLIENT_ID'),config('UPSTOX_REDIRECT_URI'))
+    return redirect(response)
 
-def get_access_token(code):
-    data = {
-        "client_id": os.environ.get("UPSTOX_CLIENT_ID"),
-        "client_secret": os.environ.get("UPSTOX_CLIENT_SECRET"),
-        "grant_type": "authorization_code",
-        "code": code,
-        "redirect_uri": os.environ.get('UPSTOX_REDIRECT_URI')
+   
+def get_access_token(request):
+    code = request.GET.get('code')
+    print(code)
+    # URL for obtaining the access token
+    token_url = 'https://api-v2.upstox.com/login/authorization/token'
+
+    # Request headers
+    headers = {
+        'accept': 'application/json',
+        'Api-Version': '2.0',
+        'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-    url = "https://discord.com/api/oauth2/token"
-    response = requests.post(url, data=data)
+    # Request data
+    data = {
+        'code': code,
+        'client_id': config('UPSTOX_CLIENT_ID'),
+        'client_secret': config("UPSTOX_CLIENT_SECRET"),
+        'redirect_uri': config('UPSTOX_REDIRECT_URI'),
+        'grant_type': 'authorization_code'
+    }
 
-    return response.json()["access_token"]
-0
-def upstox(request):
-  pdb.set_trace()
+    # Making the POST request
+    response = requests.post(token_url, headers=headers, data=data)
+
+    # Print the response
+    print(response.json())
+    if response.status_code == 200:
+        # print('Access Token:',response.json().get('access_token'))
+        access_token = response.json().get('access_token')
+        print('#########################')
+        print(access_token)
+        request.session['access_token'] = access_token
+        
+    else:
+        #Request Failed 
+        print('Error:' ,response.status_code,response.text)
+
+    # return HttpResponse('given')
+    return redirect('get_user_profile')
+ 
+
 
 def get_user_profile(request):
+    access_token = request.session.get('access_token')
     api_endpoint = 'https://api-v2.upstox.com/user/profile'
     headers = {
         'accept': 'application/json',
@@ -123,6 +77,7 @@ def get_user_profile(request):
 
 
 def get_orders(request):
+    access_token = request.session.get('access_token')
     api_endpoint = 'https://api-v2.upstox.com/order/retrieve-all'
     headers = {
         'accept': 'application/json',
@@ -142,6 +97,7 @@ def get_orders(request):
 
 
 def each_order(request,id):
+    access_token = request.session.get('access_token')
     api_endpoint = f'https://api-v2.upstox.com/order/history?tag={id}'
     headers = {
         'accept': 'application/json',
